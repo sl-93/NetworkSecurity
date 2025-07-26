e49e086700994c92a5c5723c512bb231c00674d6
sl-93 <slotfi72@gmail.com>
2025-07-26 13:20:09 +0330

    Data transformation completed!

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.logging.logger import logging
 import sys
@@ -14,14 +15,19 @@ if __name__ == '__main__':
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         logging.info("Data Ingestion completed!")
-        print(dataingestionartifact)
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data validation completed!")
-        print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact,
+                                                 data_transformation_config)
+        logging.info("Initiate data transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data transformation completed!")
         
+        print(data_transformation_artifact)
         
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -5,6 +5,7 @@ class DataIngestionArtifact:
     trained_file_path: str
     test_file_path: str
 
+
 @dataclass
 class DataValidationArtifact:
     validation_status: bool
@@ -13,3 +14,10 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
