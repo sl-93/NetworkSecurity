cb7ef3aaa8476469ab63c97b961cc2802c5ef959
sl-93 <slotfi72@gmail.com>
2025-07-29 11:52:39 +0330

    batch prediction completed!

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.pipeline.training_pipeline import TrainingPipeline
 from networksecurity.utils.main_utils.utils import load_object
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.constants.training_pipeline import (DATA_INGESTION_DATABASE_NAME, 
                                                          DATA_INGESTION_COLLECTION_NAME)
 
@@ -13,6 +14,7 @@ from fastapi import FastAPI, File, UploadFile, Request
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 import pandas as pd
 
 
@@ -34,6 +36,8 @@ app.add_middleware(
     allow_headers = ["*"]
 )
 
+templates = Jinja2Templates(directory="./templates")
+
 @app.get("/", tags = ["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -46,6 +50,24 @@ async def train_route():
         return Response("Training is successful!")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+
+@app.post("/predict")
+async def predict_route(request:Request, file:UploadFile=File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        preprcessor = load_object("final_models/preprocessor.pkl")
+        final_model = load_object("final_models/model.pkl")
+        network_model = NetworkModel(preprcessor, final_model)
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        
+        df['predicted_coolumn'] = y_pred
+        df.to_csv("prediction_output/output.csv")
+        table_html = df.to_html(classes = 'table table-striped')
+        return templates.TemplateResponse("table.html", {"request": request, "table": table_html})
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
     
 if __name__ == "__main__":
     app_run(app, host="localhost", port=8000)
